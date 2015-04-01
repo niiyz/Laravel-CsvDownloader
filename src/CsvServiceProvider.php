438d4bdac24f2c906b5a35f932d3b8535beb9b70
niiyz <yoshida@niiyz.com>
2015-04-01 23:42:46 +0900

    class name.

--- src/CsvServiceProvider.php
+++ src/CsvServiceProvider.php
@@ -9,7 +9,7 @@ class CsvServiceProvider extends ServiceProvider
     {
         $this->app->bindshared('csv', function()
         {
-            return new CSV;
+            return new Csv;
         });
 
     }
